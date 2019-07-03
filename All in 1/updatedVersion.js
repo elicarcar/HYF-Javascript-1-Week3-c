f2fa699ab07359043290d7e161afcdecd111b6bd
Eli <49886550+elicarcar@users.noreply.github.com>
2019-07-03 14:48:27 +0200

    Update updatedVersion.js

--- All in 1/updatedVersion.js
+++ All in 1/updatedVersion.js
@@ -100,25 +100,19 @@ console.log(thirdElement);
 //MJ-9.change-func-of-vehicle
 /* Change the function vehicle to use the list of question 7. 
 So that vehicle("green", 3, 1) prints "a green new bike". */
-const vehicleType = (color, age, vehicle) => {
-  if (vehicle == 1) {
-    vehicle = " bike";
-  } else if (vehicle == 2) {
-    vehicle = " car";
-  } else if (vehicle == 3) {
-    vehicle = " motorbike";
-  } else if (vehicle == 4) {
-    vehicle = " plane";
+const vehicles = ["motorbike", "car", "bus", "plane", "caravan"];
+const vehicleType = (color, index, age) => {
+  usedCar = "used";
+  newCar = "new";
+  const vehicle = vehicles[index];
+  if (age > 0) {
+    age = usedCar;
+  } else {
+    age = newCar;
   }
-  if (age > 1 ) {
-    age = "new";
-  }
-  if (age == 1) {
-    age = "used";
-  }
-
-  return "a " + color + age + vehicle;
+  return `a ${color} ${age} ${vehicle}`;
 };
+
 console.log(vehicleType("green ", 3, 1));
 
 //MJ-10.advertisement
